e992fc8ab5571ae6972780c2500f77de7a18bfe1
BananaData <46016887+BananaData@users.noreply.github.com>
2018-12-20 16:22:48 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
+//For command line Git
+// GitHub modification
+
 print("Hello world")
 print("Tell your world")
-<<<<<<< HEAD
 print("Tell his world")
-=======
 print("Tell her world")
->>>>>>> hotfix
